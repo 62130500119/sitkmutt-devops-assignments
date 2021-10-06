a131368d20519a2f53cb3a8ee0b692c6ecee4264
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-10-06 13:24:33 +0000

    Complete Assignment 4

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -2,4 +2,4 @@
 
 docker run --name reviews -p 8082:8082 --link ratings -e 'ENABLE_RATINGS=true' -e 'RATINGS_SERVICE=http://ratings:8080' reviews
 
-docker run --name productpage -p 8083:8083 --link detail -e 'DETAILS_HOSTNAME=http://detail:8081' --link rating -e 'RATINGS_HOSTNAME=http://rating:8080' --link review -e 'REVIEWS_HOSTNAME=http://review:8082' productpage
+docker run --name productpage -p 8083:8083 --link details -e 'DETAILS_HOSTNAME=http://details:8081' --link ratings -e 'RATINGS_HOSTNAME=http://ratings:8080' --link reviews -e 'REVIEWS_HOSTNAME=http://reviews:8082' productpage
